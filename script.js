b61a0822e89175043437ca9f904dc2a8e7b9bf3c
2010Rebles <lucas.horst05@escola.pr.gov.br>
2025-09-04 13:38:57 +0000

    aula 5

---
+++ script.js
@@ -0,0 +1,8 @@
+const avanca =ducument.querySelectorAAll(".btn-proximo")
+avanca.forEach(button =>{
+    button.addEvantListener("click"function(){
+    const atual = documento.querySelector("ativo");
+    const proximoPosso = 'passo-' + this.getAtribute('dta-proximo');
+
+    atual.classList.remove('ativo');
+    documento.gatElemant
